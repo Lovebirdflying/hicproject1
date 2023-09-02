2bd8b247a759333c774610e4c048600f1f9a172e
Lovebirdflying <gomitoguns@gmail.com>
2023-09-02 18:37:16 +0800

    additional code

--- project6.js
+++ project6.js
@@ -12,14 +12,31 @@
 // let check =  Array.isArray(checkArray) && checkArray.length
 //  console.log(check)
 
-     
+function isEmptyObject(obj) {
+
+    return Object.keys(obj).length === 0;
+
+}
+
+ 
+
+const sampleInput = {};
+
+const isEmpty = isEmptyObject(sampleInput);
+
+ 
+
+console.log(isEmpty); // This will output: true
+
+/*
 
 function m(arr){
     let empty = {};
-    if( Array.isArray(empty) && empty.length){
+    if( Array.isArray(arr) && arr.length){
         return false;
     }else{ return true}
 }
 var o = m({});
 console.log("6. Bolean value of an empty object :", o)
 
+*/
